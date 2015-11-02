709a9981b369f78d18c1031ea660640646057185
Huo Linhe <huolinhe@berrygenomics.com>
2015-11-02 12:58:28 +0800

    Add mainloop example like a R client.

---
+++ examples/00-mainloop.rs
@@ -0,0 +1,20 @@
+extern crate libr;
+use std::env;
+use std::ffi::CString;
+use libr::ffi::embedded::Rf_initialize_R;
+use libr::ffi::interface::*;
+
+fn main() {
+    if let Err(_) = env::var("R_HOME") {
+        panic!("Rembedded test need R_HOME be setted");
+    }
+    let mut args = env::args()
+                       .map(|arg| CString::new(arg.as_bytes()).unwrap().into_raw())
+                       .collect::<Vec<_>>();
+    unsafe {
+        R_running_as_main_program = 1;
+        Rf_initialize_R(args.len() as i32, args.as_mut_ptr());
+        Rf_mainloop();
+        return;
+    }
+}
