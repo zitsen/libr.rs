b5d1f6b4eb2a4e93ce16fa32cc5bf91fe528586a
Huo Linhe <huolinhe@berrygenomics.com>
2015-11-02 11:04:02 +0800

    Completed with R_ext/Parse.h with ext/parse.rs

--- src/ffi/ext/parse.rs
+++ src/ffi/ext/parse.rs
@@ -1,1 +1,18 @@
-//! Unimplemented.
+use super::super::internals::SEXP;
+#[repr(C)]
+#[derive(Copy, Clone, Debug)]
+pub enum ParseStatus {
+    PARSE_NULL = 0,
+    PARSE_OK,
+    PARSE_INCOMPLETE,
+    PARSE_ERROR,
+    PARSE_EOF,
+}
+
+extern {
+    pub fn R_ParseVector(arg1: SEXP,
+                         arg2: ::libc::c_int,
+                         arg3: *mut ParseStatus,
+                         arg4: SEXP)
+                         -> SEXP;
+}
