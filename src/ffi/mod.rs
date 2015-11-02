4613aa0e41f304e37cb2f772e651d61092621516
Huo Linhe <huolinhe@berrygenomics.com>
2015-11-02 09:03:25 +0800

    Add ffi TODO modules

--- src/ffi/mod.rs
+++ src/ffi/mod.rs
@@ -1,1 +1,9 @@
+pub mod config;
+pub mod defines;
+pub mod ext;
 pub mod math;
+pub mod interface;
+pub mod internals;
+pub mod s;
+pub mod r;
+
---
+++ src/ffi/r.rs
@@ -0,0 +1,17 @@
+pub use super::ext::arith::*;
+pub use super::ext::boolean::*;
+pub use super::ext::complex::*;
+pub use super::ext::error::*;
+pub use super::ext::memory::*;
+pub use super::ext::print::*;
+pub use super::ext::random::*;
+pub use super::ext::utils::*;
+pub use super::ext::rs::*;
+
+#[link(name = "R")]
+extern "C" {
+    pub fn R_FlushConsole() -> ();
+    pub fn R_ProcessEvents() -> ();
+}
+
+
