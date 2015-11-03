478aed81d2e78d87db6352bbb4f60c278f3d9a26
Huo Linhe <linhehuo@gmail.com>
2015-11-04 00:29:16 +0800

    Cargo fmt

--- build.rs
+++ build.rs
@@ -3,7 +3,6 @@ extern crate pkg_config;
 fn main() {
     match pkg_config::find_library("libR") {
         Ok(..) => return,
-        Err(e) => panic!("Couldn't find libR from \
-                           pkgconfig ({:?}), failed", e),
+        Err(e) => panic!("Couldn't find libR from pkgconfig ({:?}), failed", e),
     }
 }
