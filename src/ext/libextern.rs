4ae903a299704e7da74f0f7763ef6ac3ad03d84d
Huo Linhe <huolinhe@berrygenomics.com>
2015-11-13 18:05:57 +0800

    Document libr::ext::libextern

--- src/ext/libextern.rs
+++ src/ext/libextern.rs
@@ -1,5 +1,4 @@
-// LibImport #
-
-// LibExport #
-
-// LibExtern extern LibImport #
+//! R_ext/libextern.h
+//!
+//! Nothing for ffi, see details in [The R Souce code]
+//! (https://github.com/wch/r-source/blob/trunk/src/include/R_ext/libextern.h)
